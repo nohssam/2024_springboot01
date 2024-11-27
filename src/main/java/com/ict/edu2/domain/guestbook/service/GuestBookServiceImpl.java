390188d53cf2acabb159ecb11b6699fcf9cef9ca
nohssam <nojm73@naver.com>
2024-11-27 12:29:41 +0900

    guestbook_list, detail

---
+++ src/main/java/com/ict/edu2/domain/guestbook/service/GuestBookServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ict.edu2.domain.guestbook.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ict.edu2.domain.guestbook.mapper.GuestBookMapper;
+import com.ict.edu2.domain.guestbook.vo.GuestBookVO;
+
+@Service
+public class GuestBookServiceImpl implements GuestBookService {
+    @Autowired
+    private GuestBookMapper guestBookMapper;
+
+    @Override
+    public List<GuestBookVO> getGuestBookList() {
+        return guestBookMapper.getGuestBookList();
+    }
+
+    @Override
+    public GuestBookVO getGuestBookDetail(String gb_idx) {
+        return guestBookMapper.getGuestBookDetail(gb_idx);
+    }
+
+}
